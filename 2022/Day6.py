8c31da8e9dea5753e68dd127668789b80ecf5869
azmcnutt <azmcnutt@gmail.com>
2022-12-06 09:01:36 -0700

    Day 6 updated

--- 2022/Day6.py
+++ 2022/Day6.py
@@ -31,21 +31,24 @@ myset = get_data(day=6, year=2022).splitlines()
 # remove line feeds from the list
 starttime = time.time()
 def findStartOfPacket(data):
-    for x in range(3, len(data)):
-        #print(f'{data[x]} : {data[x-3:x]} - {data[x-1]} : {data[x-3:x-1]} - {data[x-2]} : {data[x-3:x-2]}')
-        if data[x] not in data[x-3:x] and data[x-1] not in data[x-3:x-1] and data[x-2] not in data[x-3:x-2]:
-            return x+1
+    # after seeing some solutions on-line, I'm going to change this up a bit
+    for x in range(0, len(data)):
+        # print(f'{data[x]} : {data[x-3:x]} - {data[x-1]} : {data[x-3:x-1]} - {data[x-2]} : {data[x-3:x-2]}')
+        # if data[x] not in data[x-3:x] and data[x-1] not in data[x-3:x-1] and data[x-2] not in data[x-3:x-2]:
+        if len(set(data[x:x+4])) == 4:
+            return x+4
     return 0
 
 def findStartOfPacket2(data):
     for x in range(13, len(data)):
         #print(f'{data[x]} : {data[x-3:x]} - {data[x-1]} : {data[x-3:x-1]} - {data[x-2]} : {data[x-3:x-2]}')
-        if (data[x] not in data[x-13:x] and data[x-1] not in data[x-13:x-1] and data[x-2] not in data[x-13:x-2]
-            and data[x-3] not in data[x-13:x-3] and data[x-4] not in data[x-13:x-4] and data[x-5] not in data[x-13:x-5]
-            and data[x-6] not in data[x-13:x-6] and data[x-7] not in data[x-13:x-7] and data[x-8] not in data[x-13:x-8]
-            and data[x-9] not in data[x-13:x-9] and data[x-10] not in data[x-13:x-10] and data[x-11] not in data[x-13:x-11]
-            and data[x-12] not in data[x-13:x-12]):
-            return x+1
+        #if (data[x] not in data[x-13:x] and data[x-1] not in data[x-13:x-1] and data[x-2] not in data[x-13:x-2]
+        #    and data[x-3] not in data[x-13:x-3] and data[x-4] not in data[x-13:x-4] and data[x-5] not in data[x-13:x-5]
+        #    and data[x-6] not in data[x-13:x-6] and data[x-7] not in data[x-13:x-7] and data[x-8] not in data[x-13:x-8]
+        #    and data[x-9] not in data[x-13:x-9] and data[x-10] not in data[x-13:x-10] and data[x-11] not in data[x-13:x-11]
+        #    and data[x-12] not in data[x-13:x-12]):
+        if len(set(data[x:x+14])) == 14:
+            return x+14
     return 0
 
 
