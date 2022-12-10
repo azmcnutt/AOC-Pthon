dd0a4db67367369ab0a52d035e4b5f1a706987ed
James M <azmcnutt@gmail.com>
2022-12-10 10:19:20 -0700

    Day10

--- 2015/Day8.py
+++ 2015/Day8.py
@@ -5,6 +5,7 @@ from pprint import pprint
 from aocd import get_data
 from aocd import submit
 from functools import cache
+from string import hexdigits
 import time
 import hashlib
 import ctypes
@@ -25,7 +26,8 @@ import ctypes
 myset = """\"\"
 \"abc\"
 \"aaa\\\"aaa\"
-\"\\x27\"""".splitlines()
+\"\\x27\"
+\"g\\\\k\\x27qsl\\x34hwfglcdxqbeclt\\xca\\\\\"""".splitlines()
 
 # once the test data provides the right answer: replace test data with data from the puzzle input
 myset = get_data(day=8, year=2015).splitlines()
@@ -36,26 +38,60 @@ len_char = 0
 
 for x in myset:
     len_code += len(x)
+    #print(f'{x} : {len(x)}')
     if x[0] == '"':
         x= x[1:]
     if x[-1] == '"':
         x = x[:-1]
-    madeChanges = True
-    while madeChanges:
-        madeChanges = False
-        for y in range(0,len(x)):
-            if x[y] == '\\' and x[y+1] == 'x' and x[y+2:y+4].isnumeric():
-                x = x[:y] + 'x' + x[y+5:]
-                madeChanges = True
-                break
-            elif x[y] == '\\':
-                x = x[:y] + x[y+1:]
-                madeChanges = True
-                break          
-    len_char += len(x) 
+    y = 0
+    tempstr = ''
+    while y < len(x):
+        #for y in range(0,len(x)):
+        if len(x) - y >= 4  and x[y] == '\\' and x[y+1] == 'x' and all(c in hexdigits for c in x[y+2:y+4]):
+            tempstr += 'X'
+            y += 4
+        elif x[y:y+1] == '\\' or x[y:y+1] == '\"':
+            tempstr += 'V'
+            y += 2
+        else:
+            tempstr += x[y]
+            y += 1
+    len_char += len(tempstr)
+    #print(f'{tempstr} : {len(tempstr)}')
+print(f'Part 1 Answer is {len_code - len_char}    {time.time() - starttime}')
+starttime = time.time()
+len_code = 0
+len_char = 0
+for x in myset:
+    extra_codes = 0
+    len_code += len(x)
+    print(f'{x} : {len(x)}')
+    if x[0] == '"':
+        x= x[1:]
+        extra_codes += 3
+    if x[-1] == '"':
+        x = x[:-1]
+        extra_codes += 3
+    y = 0
+    tempstr = ''
+    while y < len(x):
+        #for y in range(0,len(x)):
+        if len(x) - y >= 4  and x[y] == '\\' and x[y+1] == 'x' and all(c in hexdigits for c in x[y+2:y+4]):
+            #tempstr += 'X'
+            extra_codes += 1
+            y += 4
+        elif x[y:y+1] == '\\' or x[y:y+1] == '\"':
+            #tempstr += 'V'
+            extra_codes += 2
+            y += 2
+        else:
+            # tempstr += x[y]
+            y += 1
+    len_char += len(x) + extra_codes
+    print(f'"{x}" : {len(x) + extra_codes}')
     
 #print(f'{len_code - len_char}')
-print(f'Part 1 Answer is {len_code - len_char}    {time.time() - starttime}')
+
 # submit(p1ans, part='a', day = 7, year=2022)
-# print(f'Part 2 Answer is {p2ans}    {time.time() - starttime}')
+print(f'Part 2 Answer is {len_char - len_code}    {time.time() - starttime}')
 # submit(p1ans, part='a', day = 7, year=2022)
---
+++ 2022/Day10.py
@@ -0,0 +1,221 @@
+from copy import deepcopy
+from pprint import pprint
+from aocd import get_data
+import time
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# --- Day 10: Cathode-Ray Tube ---                                                                        #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Part 1                                                                                                        #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                                                                               #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Part 2                                                                                                        #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                                                                               #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# load sample data, copied and pasted from the site into list.  Each list item is one line of input
+myset = """addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop""".splitlines()
+
+# once the test data provides the right answer: replace test data with data from the puzzle input
+myset = get_data(day=10, year=2022).splitlines()
+
+starttime = time.time()
+crt = ''
+cycles = 0
+X = 1
+signal = 0
+
+def calcSignal():
+    global crt
+    global signal
+    if cycles == 20 or (cycles > 40 and (cycles - 20) % 40 == 0):
+        # print(f'Signal: {sig} += Cycle: {cycles} * X:{X} = {cycles * X}')
+        signal += (cycles * X)
+    crt_pos = 40 if cycles % 40 == 0 else cycles % 40
+    if crt_pos >= X and crt_pos <= X + 2:
+        crt += b'\xdb'.decode('cp437')
+    else:
+        crt += ' '
+    if cycles % 40 == 0:
+        #print(crt)
+        crt += '\r\n'
+
+
+
+for i in myset:
+    if i == 'noop':
+        cycles += 1
+        calcSignal()
+    else:
+        _,y = i.split(' ')
+        #cycles += 1
+        #calcSignal()
+        #cycles += 1
+        #calcSignal()
+        for _ in (1,2):
+            cycles += 1
+            calcSignal()
+        X += int(y)
+        
+
+
+
+
+
+
+
+
+
+
+print(f'Part 1 Answer is {signal}    {time.time() - starttime}')
+# submit(p1ans, part='a', day = 7, year=2022)
+print(f'Part 2 Answer is:')
+print(crt)
+# submit(p1ans, part='a', day = 7, year=2022)
