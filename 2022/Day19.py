bfc92422bf2177b471601ef664d26bc2208ccd98
James M <azmcnutt@gmail.com>
2022-12-18 15:00:41 -0700

    Ready for tomorrow.

---
+++ 2022/Day19.py
@@ -0,0 +1,33 @@
+from copy import deepcopy
+from pprint import pprint
+from aocd import get_data
+from time import time
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# --- Day 17:  ---                                                                        #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Part 1                                                                                                        #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                                                                               #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Part 2                                                                                                        #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                                                                               #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+# load sample data, copied and pasted from the site into list.  Each list item is one line of input
+myset = """""".splitlines()
+
+# once the test data provides the right answer: replace test data with data from the puzzle input
+# myset = get_data(day=19, year=2022).splitlines()
+
+# for i,d in enumerate(myset):
+#    print(f'{i}: {d}')
+
+starttime = time()
+
+
+
+print(f'Part 1 Answer is: {p1ans}   {time() - starttime}')
+
