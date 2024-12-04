a0248508b4cc1c70d4f89d6e107cd205d1adf9ca
James McNutt <azmcnutt@gmail.com>
2024-12-03 22:29:35 -0700

    2024 Day 4: Brute force not working

--- 2024/Day4.py
+++ 2024/Day4.py
@@ -8,19 +8,28 @@ import time
 
 
 def main():
-    # # # # # # # # # # # #
-    # --- Day 2: TBD  --- #
-    # # # # # # # # # # # #
+    # # # # # # # # # # # # # # # # #
+    # --- Day 4: Ceres Search  ---  #
+    # # # # # # # # # # # # # # # # #
 
     # load sample data, copied and pasted from the site into list.
     # Each list item is one line of input
-    aoc_input = """Sample
-    Data""".splitlines()
+    aoc_input = """MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX""".splitlines()
 
 
     # once the test data provides the right answer:
     # replace test data with data from the puzzle input
-    # aoc_input = get_data(day=4, year=2024).splitlines()
+    aoc_input = get_data(day=4, year=2024).splitlines()
+    # Not right 2492
 
     # Get the time to see how fast the solution runs.
     # I get the time after the input has been downloaded to test
@@ -29,6 +38,64 @@ def main():
 
     p1 = 0
     p2 = 0
+
+    for x in range(len(aoc_input)):
+        for y in range(len(aoc_input[x])):
+            # Check for xmas forward
+            if aoc_input[x][y:y+4] == 'XMAS':
+                p1 += 1
+            # Check for xmas backwards
+            if aoc_input[x][y:y-4:-1] == 'XMAS':
+                p1 += 1
+            # Check for xmas down
+            if x + 3 <= len(aoc_input) - 1:
+                if (aoc_input[x][y] == 'X' and
+                    aoc_input[x+1][y] == 'M' and
+                    aoc_input[x+2][y] == 'A' and
+                    aoc_input[x+3][y] == 'S'
+                ):
+                    p1 += 1
+            # Check for xmas up
+            if x - 3 >= 0:
+                if (aoc_input[x][y] == 'X' and
+                    aoc_input[x-1][y] == 'M' and
+                    aoc_input[x-2][y] == 'A' and
+                    aoc_input[x-3][y] == 'S'
+                ):
+                    p1 += 1
+            # Check for xmas down to the right
+            if x + 3 <= len(aoc_input) - 1 and y + 3 <= len(aoc_input[x]) - 1:
+                if (aoc_input[x][y] == 'X' and
+                    aoc_input[x+1][y+1] == 'M' and
+                    aoc_input[x+2][y+2] == 'A' and
+                    aoc_input[x+3][y+3] == 'S'
+                ):
+                    p1 += 1
+            # Check for xmas down to the left
+            if x + 3 <= len(aoc_input) - 1 and y - 3 >= 0:
+                if (aoc_input[x][y] == 'X' and
+                    aoc_input[x+1][y-1] == 'M' and
+                    aoc_input[x+2][y-2] == 'A' and
+                    aoc_input[x+3][y-3] == 'S'
+                ):
+                    p1 += 1
+            # Check for xmas up to the right
+            if x - 3 >= 0 and y + 3 <= len(aoc_input[x]) - 1:
+                if (aoc_input[x][y] == 'X' and
+                    aoc_input[x-1][y+1] == 'M' and
+                    aoc_input[x-2][y+2] == 'A' and
+                    aoc_input[x-3][y+3] == 'S'
+                ):
+                    p1 += 1
+            # Check for xmas up to the left
+            if x - 3 >= 0 and y - 3 >= 0:
+                if (aoc_input[x][y] == 'X' and
+                    aoc_input[x-1][y-1] == 'M' and
+                    aoc_input[x-2][y-2] == 'A' and
+                    aoc_input[x-3][y-3] == 'S'
+                ):
+                    p1 += 1
+
 
     print(f'P1: {p1}, P2: {p2} in {time.time() - start_time} seconds.')
 
