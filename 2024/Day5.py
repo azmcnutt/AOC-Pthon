b27372c0610e8b4da2391885365ac49c748d7dea
James M <azmcnutt@gmail.com>
2024-12-05 07:41:45 -0700

    2024 Day 5: Started working too soon

--- 2024/Day5.py
+++ 2024/Day5.py
@@ -1,7 +1,7 @@
 # import os
 # import sys
 # import copy
-# from pprint import pprint
+from pprint import pprint
 from aocd import get_data
 # from aocd import submit
 import time
@@ -29,6 +29,28 @@ def main():
 
     p1 = 0
     p2 = 0
+
+    rules = {}
+    pages = []
+    switch = False
+    for line in aoc_input:
+        if not switch:
+            # We are still in the rules section
+            if not line:
+                switch = True
+                continue
+            x,y = line.split('|')
+            if x in rules.keys():
+                rules[x].append(y)
+            else:
+                rules[x] = [y,]
+        else:
+            pages.append(line.split(','))
+    
+    pprint(rules)
+    print()
+    pprint(pages)
+
 
     print(f'P1: {p1}, P2: {p2} in {time.time() - start_time} seconds.')
 
