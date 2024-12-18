f3d6a24b2a803c83c55665b1b9799d452134e260
James M <azmcnutt@gmail.com>
2024-12-18 16:02:24 -0700

    2024 Day 17: Part one complete, brute force part 2

---
+++ 2024/Day17.py
@@ -0,0 +1,160 @@
+# import os
+# import sys
+# import copy
+# from pprint import pprint
+from aocd import get_data
+# from aocd import submit
+import time
+
+reg_a = 0
+reg_b = 0
+reg_c = 0
+pointer = 0
+
+def main():
+    global reg_a
+    global reg_b
+    global reg_c
+    global pointer
+    # # # # # # # # # # # # # # # # # # # # #
+    # --- Day 0: Chronospatial Computer --- #
+    # # # # # # # # # # # # # # # # # # # # #
+
+    # load sample data, copied and pasted from the site into list.
+    # Each list item is one line of input
+    aoc_input = """Register A: 729
+Register B: 0
+Register C: 0
+
+Program: 0,1,5,4,3,0""".splitlines()
+    
+    aoc_input = """Register A: 2024
+Register B: 0
+Register C: 0
+
+Program: 0,3,5,4,3,0""".splitlines()
+
+    # once the test data provides the right answer:
+    # replace test data with data from the puzzle input
+    aoc_input = get_data(day=17, year=2024).splitlines()
+
+    # Get the time to see how fast the solution runs.
+    # I get the time after the input has been downloaded to test
+    # the speed of my program, not the speed of my Internet connection.
+    start_time = time.time()
+
+    p1 = ''
+    p2 = 1
+    program = []
+    original_program = ''
+
+    for x in aoc_input:
+        if x[0:12] == "Register A: ":
+            print(f'Reg A: {x[12:]}')
+            reg_a = int(x[12:])
+        elif x[0:12] == "Register B: ":
+            print(f'Reg B: {x[12:]}')
+            reg_b = int(x[12:])
+        elif x[0:12] == "Register C: ":
+            print(f'Reg C: {x[12:]}')
+            reg_c = int(x[12:])
+        elif x[0:9] == "Program: ":
+            print(f'Program: {x[9:]}')
+            original_program = x[9:]
+            temp = x[9:].split(',')
+            program = [int(item) for item in temp]
+    
+    while pointer < len(program):
+        ret = instructions(program[pointer], program[pointer + 1])
+        if ret is not None:
+            if not p1:
+                p1 = str(ret)
+            else:
+                p1 += ',' + str(ret)
+    
+    # part 2 brute force:
+    p2 = 999999999999999
+    # for _ in range(1, 1000):
+    while True:
+        pointer = 0
+        ans = ''
+        reg_a = p2
+        reg_b = 0
+        reg_c = 0
+        program = [int(item) for item in temp]
+        while pointer < len(program):
+            ret = instructions(program[pointer], program[pointer + 1])
+            if ret is not None:
+                if not ans:
+                    ans = str(ret)
+                else:
+                    ans += ',' + str(ret)
+        # print(f'{p2}: {ans} ({original_program})')
+        if ans == original_program:
+            break
+        p2 += 1
+
+    print(f'P1: {p1} - P2: {p2} in {time.time() - start_time} seconds.')
+
+def instructions(opcode, operand):
+    global reg_a
+    global reg_b
+    global reg_c
+    global pointer
+    if opcode == 0:
+        operand = convert_combo(operand)
+        operand = 2 ** operand
+        reg_a = int(reg_a / operand)
+        pointer += 2
+        return
+    if opcode == 1:
+        reg_b = reg_b ^ operand
+        pointer += 2
+        return
+    if opcode == 2:
+        operand = convert_combo(operand)
+        reg_b = operand % 8
+        pointer += 2
+        return
+    if opcode == 3:
+        if reg_a == 0:
+            pointer += 2
+            return
+        else:
+            pointer = operand
+            return
+    if opcode == 4:
+        reg_b = reg_b ^ reg_c
+        pointer += 2
+        return
+    if opcode == 5:
+        operand = convert_combo(operand)
+        pointer += 2
+        return operand % 8
+    if opcode == 6:
+        operand = convert_combo(operand)
+        operand = 2 ** operand
+        reg_b = int(reg_a / operand)
+        pointer += 2
+        return
+    if opcode == 7:
+        operand = convert_combo(operand)
+        operand = 2 ** operand
+        reg_c = int(reg_a / operand)
+        pointer += 2
+        return
+
+def convert_combo(o):
+    if 0 <= o <= 3:
+        return o
+    elif o == 4:
+        return reg_a
+    elif o == 5:
+        return reg_b
+    elif o == 6:
+        return reg_c
+    return None
+
+if __name__ == '__main__':
+    main()
+ 
