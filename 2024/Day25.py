7123c41da0d2bffbb075cfe33761150d72a14494
James McNutt <azmcnutt@gmail.com>
2024-12-24 22:32:29 -0700

    2024 Day 25: Complete - 50 Stars

--- 2024/Day25.py
+++ 2024/Day25.py
@@ -58,7 +58,7 @@ def main():
 
     # once the test data provides the right answer:
     # replace test data with data from the puzzle input
-    # aoc_input = get_data(day=25, year=2024).splitlines()
+    aoc_input = get_data(day=25, year=2024).splitlines()
 
     # Get the time to see how fast the solution runs.
     # I get the time after the input has been downloaded to test
@@ -67,11 +67,40 @@ def main():
 
     p1 = 0
     p2 = 0
-
-    locks = {}
-    keys = {}
-
     
+    aoc_input.append([])
+    locks = []
+    keys = []
+    temp = []
+    max_height = 0
+    for a in aoc_input:
+        if a:
+            temp.append(a)
+        else:
+            if not max_height:
+                max_height = len(temp)
+            b = [0] * len(temp[0])
+            for c in temp:
+                for i, d in enumerate(c):
+                    if d == '#':
+                        b[i] += 1
+            if temp[0].startswith('#'):
+                locks.append(b)
+            elif temp[-1].startswith('#'):
+                keys.append(b)
+            temp = []
+    
+    for k in keys:
+        for l in locks:
+            possible_match = True
+            for i, _ in enumerate(k):
+                if max_height - k[i] < l[i]:
+                    possible_match = False
+                    break
+            if possible_match:
+                p1 += 1
+
+
 
     print(f'P1: {p1}, P2: {p2} in {time.time() - start_time} seconds.')
 
